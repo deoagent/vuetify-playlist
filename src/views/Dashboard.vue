f5157944ec426299ec91a14ff596f778afd492cf
deoagent <lbteksystems@gmail.com>
2022-06-23 22:55:10 +0800

    lesson 13 Grid System Part 2

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,29 +3,28 @@
     <h1 class="subheading grey--text">Dashboard</h1>
 
     <v-container class="my-5">
-        <v-layout row wrap>
-          <v-flex xs12 md6>
-            <v-btn outline block class="primary">1</v-btn>
-          </v-flex>
-          <v-flex xs4 md2>
-            <v-btn outline block class="primary">2</v-btn>
-          </v-flex>
-          <v-flex xs4 md2>
-            <v-btn outline block class="primary">2</v-btn>
-          </v-flex>
-          <v-flex xs4 md2>
-            <v-btn outline block class="primary">2</v-btn>
-          </v-flex>
-        </v-layout>
 
-        <v-layout row wrap justify-space-around>
-          <v-flex xs4 md3>
-            <v-btn outline block class="success">1</v-btn>
-          </v-flex>
-          <v-flex xs4 md3>
-            <v-btn outline block class="success">2</v-btn>
-          </v-flex>
-        </v-layout>
+        <v-card flat class="pa-3">
+            <v-layout row wrap>
+              <v-flex xs12 md6>
+                <div class="caption grey--text">Project Title</div>
+                <div>Create a new website</div>
+              </v-flex>
+              <v-flex xs6 sm4 md2>
+                <div class="caption grey--text">Person</div>
+                <div>The Net Ninja</div>
+              </v-flex>
+              <v-flex xs6 sm4 md2>
+                <div class="caption grey--text">Due by</div>
+                <div>1st Jan. 2022</div>
+              </v-flex>
+              <v-flex xs6 sm4 md2>
+                <div class="caption grey--text">Status</div>
+                <div>ongoing</div>
+              </v-flex>
+            </v-layout>
+        </v-card>
+
     </v-container>
 
   </div>
