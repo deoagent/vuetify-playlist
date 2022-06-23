d50628f2d49ca5cd6df11822356574302d51780d
deoagent <lbteksystems@gmail.com>
2022-06-23 17:55:41 +0800

    all Lessons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,7 @@
 <template>
   <div>
     <h1>Homepage</h1>
-      <v-btn class="pink white--text">click me</v-btn>
-      <v-btn depressed dark color="pink">click me</v-btn>
-      <v-btn flat color="pink">click me</v-btn>
-
-      <v-btn depressed class="pink white--text">
-        <v-icon left>email</v-icon>
-        <span>email me</span>
-      </v-btn>
-
-      <v-btn depressed small class="pink white--text">
-        <v-icon left small>email</v-icon>
-        <span>email me</span>
-      </v-btn>
-
-      <v-btn depressed large class="pink white--text">
-        <span>email me</span>
-        <v-icon right small>email</v-icon>
-      </v-btn>
-
-      <v-btn fab depressed small dark color="purple">
-        <v-icon >favorite</v-icon>
-      </v-btn>
-
+    <p>https://github.com/iamshaunjp/vuetify-playlist/tree/lesson-2/todo-ninja</p>
   </div>
 </template>
 
