a40c1c1d77633203276614a433aa942bb0a0f147
deoagent <lbteksystems@gmail.com>
2022-06-23 18:44:45 +0800

    lesson 8

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
     <nav>
         <v-toolbar flat app>
+            <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
             <v-toolbar-title class="text-uppercase grey--text">
                 <span class="font-weight-light">Todo</span>
                 <span>Ninja</span>
@@ -11,12 +12,23 @@
                 <v-icon right>exit_to_app</v-icon>
             </v-btn>
         </v-toolbar>
+
+        <v-navigation-drawer v-model="drawer" app class="indigo">
+            <p>test</p>
+        </v-navigation-drawer>
+
     </nav>
 </template>
 
 
 <script>
 export default {
+    data() {
+        return{
+            drawer: false
+        }
+        
+    }
 
 }
 </script>
