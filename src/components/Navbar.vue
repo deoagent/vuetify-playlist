3d5545b88dffa47c0bfad880941b780ba7d4f674
deoagent <lbteksystems@gmail.com>
2022-06-23 18:31:06 +0800

    lesson 7

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,17 @@
 <template>
-    <nav>Hello</nav>
+    <nav>
+        <v-toolbar flat app>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light">Todo</span>
+                <span>Ninja</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn flat color="grey">
+                <span>Sign Out</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+        </v-toolbar>
+    </nav>
 </template>
 
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,6 @@
   <div>
     <h1>Homepage</h1>
 
-    <!-- <v-btn class="hidden-md-and-down">click me</v-btn> -->
-
-    <!-- <v-btn class="hidden-md-and-up">click me</v-btn> -->
-
-    <v-btn class="hidden-sm-only">click me</v-btn>
 
   </div>
 </template>
