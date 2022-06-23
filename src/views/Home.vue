ae1e6ce05c7aacddf3b37e20282f28f8a7b7b1d7
deoagent <lbteksystems@gmail.com>
2022-06-23 17:35:02 +0800

    lesson 2

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. In illum, inventore et unde, animi eos pariatur quibusdam quam obcaecati, molestiae repudiandae qui porro? Harum quas eius commodi accusantium facilis error.</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam cupiditate aliquid aliquam ex aspernatur laudantium temporibus in, consectetur voluptatum beatae voluptatibus consequatur voluptas repellendus amet, unde quasi deleniti vitae corporis?</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h1 class="display-1">Smaller Display</h1>
+    <p class="headline">This is a headline</p>
+    <p class="subheading">This is a subheading</p>
+    <p class="caption"></p>
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+  }
+</script>
