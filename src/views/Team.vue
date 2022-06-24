b6f1c03333fadcdb766fed81f504a72b0f007631
deoagent <lbteksystems@gmail.com>
2022-06-24 11:23:24 +0800

    Lesson 19 Cards

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -3,7 +3,25 @@
     <h1 class="subheading grey--text">Team</h1>
 
     <v-container class="my-5">
-        content
+        <v-layout row wrap>
+            <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+                <v-card class="text-xs-center ma-3">
+                    <v-responsive class="pt-4">
+                        image goes here
+                    </v-responsive>
+                    <v-card-text>
+                        <div class="subheading">{{person.name}}</div>
+                        <div class="grey--text">{{person.role}}</div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn flat color="grey">
+                            <v-icon small left>message</v-icon>
+                            <span>Message</span>
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
     </v-container>
 
 </div>
@@ -13,5 +31,16 @@
 
 
 export default {
+    data() {
+        return {
+            team: [
+                {name: 'The Net Ninja', role: 'Web developer'},
+                {name: 'Ryu', role: 'Graphic designer'},
+                {name: 'Chun Li', role: 'Web developer'},
+                {name: 'Gouken', role: 'Social media maverick'},
+                {name: 'Yoshi', role: 'Sales guru'},
+            ]
+        }
+    }
 }
 </script>
