403f75323b3f1d33e96b782cb6bbbdbebd9ad08e
deoagent <lbteksystems@gmail.com>
2022-06-24 10:32:04 +0800

    Lesson 17 Sorting Projects

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,6 +4,17 @@
 
     <v-container class="my-5">
 
+      <v-layout row class="mb-3">
+        <v-btn small flat color="grey" @click="sortBy('title')">
+          <v-icon left small>folder</v-icon>
+          <span class="caption text-lowercase">By project name</span>
+        </v-btn>
+        <v-btn small flat color="grey" @click="sortBy('person')">
+          <v-icon left small>person</v-icon>
+          <span class="caption text-lowercase">By project person</span>
+        </v-btn>
+      </v-layout>
+
         <v-card flat  v-for="project in projects" :key="project.title">
             <v-layout row wrap :class="`pa-3 project ${project.status}`">
               <v-flex xs12 md6>
@@ -44,6 +55,11 @@
           { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
           { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
       ]
+      }
+    },
+    methods: {
+      sortBy(prop){
+        this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
       }
     }
   }
